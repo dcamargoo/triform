a2ecb814c6d8158b7826aaadc80ce886714ff389
Daniel <danrcamargo.code@gmail.com>
2026-02-15 12:50:15 -0300

    Melhorando a qualidade da mesh com otimizações e remoção de outliers

--- reconstruction/meshing.py
+++ reconstruction/meshing.py
@@ -1,7 +1,8 @@
 import open3d as o3d
 import os
+import numpy as np
 
-# gerando a malha com Poisson Surface Reconstruction, usando a nuvem de pontos densa gerada pelo MVS
+# gerando a malha com Poisson Surface Reconstruction, usando a nuvem de pontos densa gerada pelo MVS e otimizando a malha resultante (removendo partes ruins e suavizando)
 def generate_mesh():
 
     FUSED_PATH = "../colmap/dense/fused.ply"
@@ -13,20 +14,42 @@ def generate_mesh():
     # capturando a nuvem de pontos densa gerada pelo MVS
     pointCloud = o3d.io.read_point_cloud(FUSED_PATH)
 
+    # removendo ruídos da nuvem
+    pointCloud, ind = pointCloud.remove_statistical_outlier(
+        nb_neighbors=20,
+        std_ratio=2.0
+    )
+
     # estima normais (orientação)
     pointCloud.estimate_normals()
 
     # gerando a malha usando Poisson Surface Reconstruction
     mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
         pointCloud,
-        depth=9
+        depth=10 # define a quantidade de polígonos na malha (resolução)
     )
+
+    # organizando as densidades para remover partes ruins da malha
+    densities = np.asarray(densities)
+    threshold = np.quantile(densities, 0.10)
+
+    vertices_to_remove = densities < threshold
+    mesh.remove_vertices_by_mask(vertices_to_remove)
+
+    mesh.remove_duplicated_vertices()
+    mesh.remove_duplicated_triangles()
+    mesh.remove_degenerate_triangles()
+    mesh.remove_non_manifold_edges()
+
+    # suavização taubin
+    mesh = mesh.filter_smooth_taubin(number_of_iterations=10)
+    mesh.compute_vertex_normals()
 
     os.makedirs("../static/models", exist_ok=True)
 
     # salvando a malha gerada
     o3d.io.write_triangle_mesh(OUTPUT_PATH, mesh)
 
-    print("Mesh gerada com sucesso!")
+    print("Mesh gerada e otimizada com sucesso!")
 
-#generate_mesh() # teste local
+generate_mesh() # teste local
